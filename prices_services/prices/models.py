0d9c2b70019d2612a6d93d2a6a5febe7615126b7
Juan Calderon <juan.calderon@appgate.com>
2024-09-10 10:35:10 -0500

    Implemented Price model and added initial data with timezone awareness

--- prices_services/prices/models.py
+++ prices_services/prices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Price(models.Model):
+    brand_id = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    price_list = models.IntegerField()
+    product_id = models.IntegerField()
+    priority = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    curr = models.CharField(max_length=10)
